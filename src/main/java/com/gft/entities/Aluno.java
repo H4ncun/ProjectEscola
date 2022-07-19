463efc725605e1f0728c0998b2052b61c3722ce5
lacu <lacu@gft.com>
2022-07-19 11:10:21 -0300

    add initial classes and update dependencies

---
+++ src/main/java/com/gft/entities/Aluno.java
@@ -0,0 +1,39 @@
+package com.gft.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.*;
+
+@Entity
+@Table(name = "tb_aluno")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @Email
+    private String email;
+
+    @CPF
+    private String cpf;
+
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", cpf=" + cpf + ", email=" + email + "]";
+    }
+}
